26df6ffc4a402051034aaf457713748345496057
kadi-hub3 <kicktheball1@gmail.com>
2024-09-27 19:13:35 +0200

    Initial page

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,35 @@
-import { Image, StyleSheet, View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native'
+import React from 'react'
+import beachImage from '@/assets/meditation-images/beach.webp'
+import {LinearGradient} from 'expo-linear-gradient'
+import {StatusBar} from 'expo-status-bar'
 
-
-export default function HomeScreen() {
+import { SafeAreaView } from 'react-native-safe-area-context'
+const App = () => {
   return (
-  <View style={styles.container}>
-      <Text>
-        Hello WO
-      </Text>
+    <View className='flex-1'>
+      <ImageBackground 
+      source={beachImage}
+      resizeMode='cover'
+      className='flex-1' >
+        <LinearGradient 
+          className='flex-1'
+          colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.8)']}>
+          <SafeAreaView className='flex-1 px-1 justify-between'>
+            <Text className='text-center text-white font-bold text-4xl'>
+              Spiritually Meditari
+            </Text>
+            <Text className='flex-1 text-white'>
+              Connect to your highest self for permanent success
+            </Text>
+          </SafeAreaView>
+          <StatusBar style='light' />
+
+        </LinearGradient>
+
+      </ImageBackground>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      "./App.{js,jsx,ts,tsx}",
+      "./app/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the app folder
+      "./components/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the components folder]
+      "./app/(tabs)/meditate.tsx",
+  ],
   theme: {
-    extend: {},
+      extend: {
+          fontFamily: {
+              rmono: ["Roboto-Mono", "sans-serif"],
+          },
+      },
   },
   plugins: [],
-}
-
+};
