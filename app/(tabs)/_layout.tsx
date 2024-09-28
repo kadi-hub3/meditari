009a51b7d6cada6744c1cd7d0c361fb3c803b316
kadi-hub3 <kicktheball1@gmail.com>
2024-09-28 18:21:13 +0200

    Tab Layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,13 +2,37 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 import Colors from '@/constants/Colors'
-
+import {MaterialCommunityIcons, Entypo} from '@expo/vector-icons'
 const TabsLayout = () => {
   return (
     <Tabs screenOptions={{headerShown:false, tabBarActiveTintColor: Colors.light}}>
         <Tabs.Screen
             name='nature-meditate'
-            options={{tabBarLabel: 'Meditate'}} />
+            options={{
+                tabBarLabel: 'Meditate',
+                tabBarIcon: ({color})=>(
+                    <MaterialCommunityIcons 
+                        name='flower-tulip'
+                        size={20}
+                        color={color}
+                    />
+                )
+            }} 
+        />
+
+        <Tabs.Screen
+            name='affirmations'
+            options={{
+                tabBarLabel: 'Affirmations',
+                tabBarIcon: ({color})=>(
+                    <Entypo 
+                        name='open-book'
+                        size={20}
+                        color={color}
+                    />
+                )
+            }} 
+        />
     </Tabs>
   )
 }
