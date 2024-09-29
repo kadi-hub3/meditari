6e0c4aacb7b88a98b1f7e958e9928eaaf5d67b9a
kadi-hub3 <kicktheball1@gmail.com>
2024-09-29 20:34:56 +0200

    Affirmations Page

--- app/(tabs)/affirmations.tsx
+++ app/(tabs)/affirmations.tsx
@@ -1,10 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import AppGradient from '@/components/AppGradient'
 
 const Affirmations = () => {
   return (
-    <View>
-      <Text>Affirmations</Text>
+    <View className='flex-1'>
+        <AppGradient colors={['#161b2e', '#0a4d4a', '#766e67']}>
+            <View>
+                <Text className='text-gray-200 mb-3 font-bold text-4xl'>Affirmations</Text>
+
+            </View>
+
+        </AppGradient>
+
     </View>
   )
 }
