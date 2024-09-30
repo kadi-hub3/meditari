a27fd9ca22d587b1d1f780040c4bfdcd386d23b0
kadi-hub3 <kicktheball1@gmail.com>
2024-09-30 16:36:01 +0200

    Affirmations

--- app/(tabs)/affirmations.tsx
+++ app/(tabs)/affirmations.tsx
@@ -1,13 +1,26 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import AppGradient from '@/components/AppGradient'
+import AFFIRMATION_GALLERY from '@/constants/affirmation-gallery'
+import GuidedAffirmationsGallery from '@/components/GuidedAffirmations'
 
 const Affirmations = () => {
   return (
     <View className='flex-1'>
-        <AppGradient colors={['#161b2e', '#0a4d4a', '#766e67']}>
+        <AppGradient colors={['#2e1f58', '#54426b', '#a790af']}>
             <View>
-                <Text className='text-gray-200 mb-3 font-bold text-4xl'>Affirmations</Text>
+                <Text className='text-gray-200 mb-3 font-bold text-3xl'>
+                    Reprogram your belief system with affirmations
+                </Text>
+            </View>
+            <View>
+                {AFFIRMATION_GALLERY.map((g)=> (
+                    <GuidedAffirmationsGallery 
+                        key={g.title}
+                        title={g.title}
+                        previews={g.data}
+                    />
+                ))}
 
             </View>
 
---
+++ components/GuidedAffirmations.tsx
@@ -0,0 +1,42 @@
+import { View, Text, Pressable, Image, FlatList } from 'react-native'
+import React from 'react'
+import { GalleryPreviewData } from '@/constants/models/AffirmationCategory';
+import { Link } from 'expo-router';
+
+interface GuidedAffirmationsProps {
+    title: string;
+    previews: GalleryPreviewData[];
+}
+
+const GuidedAffirmationsGallery = ({title, previews}: GuidedAffirmationsProps) => {
+  return (
+    <View className='my-5'>
+        <View className='mb-2'>
+            <Text className='text-white text-xl font-bold'>{title}</Text>
+        </View>
+        <View className='space-y-2'>
+            <FlatList 
+                data={previews}
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={(item)=>item.id.toString()}
+                renderItem={({item, index})=> (
+                    <Link href={`/affirmations/${index}`}  asChild>
+                        <Pressable>
+                            <View className='h-32 w-28 rounded-md'>
+                                <Image 
+                                    source={item.image}
+                                    resizeMode='cover'
+                                    className='w-full h-full'
+                                />
+                            </View>
+                        </Pressable>
+                    </Link>
+                )}
+                horizontal
+            />
+        </View>
+    </View>
+  )
+}
+
+export default GuidedAffirmationsGallery
