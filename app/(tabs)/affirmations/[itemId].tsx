c869e819a64eeabddd815a1c33dc21de6ad4ce55
kadi-hub3 <kicktheball1@gmail.com>
2024-09-30 17:45:26 +0200

    Affirmations

--- app/(tabs)/affirmations/[itemId].tsx
+++ app/(tabs)/affirmations/[itemId].tsx
@@ -1,4 +1,4 @@
-import { View, Text, ImageBackground, Pressable } from 'react-native'
+import { View, Text, ImageBackground, Pressable, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useLocalSearchParams, router } from 'expo-router'
 import { GalleryPreviewData } from '@/constants/models/AffirmationCategory';
@@ -43,8 +43,24 @@ const SelectedAffirmation = () => {
                 >
                         <AntDesign name="leftcircleo" size={50} color="white" />
                 </Pressable>
+                <ScrollView
+                        className="mt-20"
+                        showsVerticalScrollIndicator={false}
+                >
+                    <View className="h-full border-white justify-center">
+                        <View className="h-4/5 justify-center">
+                                {sentences.map((sentence, idx) => (
+                                    <Text
+                                        className="text-white text-3xl mb-12 font-bold text-center"
+                                        key={idx}
+                                    >
+                                        {sentence}.
+                                    </Text>
+                                ))}
+                        </View>
+                    </View>
+                </ScrollView>
             </AppGradient>
-
         </ImageBackground>
     </View>
   )
