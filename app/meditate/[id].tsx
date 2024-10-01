2e1facf5a438c8059c2530123beeac7d2708677c
kadi-hub3 <kicktheball1@gmail.com>
2024-10-01 17:00:28 +0200

    Meditatte indexed page

--- app/meditate/[id].tsx
+++ app/meditate/[id].tsx
@@ -25,9 +25,15 @@ const Meditate = () => {
             >
                 <AntDesign name="leftcircleo" size={50} color="white" />
             </Pressable>
+            <View className='flex-1 justify-center'>
+              <View className='mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center'>
+                <Text className='text-4xl text-green-950 font-rmono'>
+                  00:00
+                </Text>
+              </View> 
+            </View>
         </AppGradient>
       </ImageBackground>
-      <Text>Meditate</Text>
     </View>
   )
 }
