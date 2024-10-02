ac935afb9c518414c40fddb6c4a9f3b6df35143b
kadi-hub3 <kicktheball1@gmail.com>
2024-10-02 16:36:48 +0200

    Font Loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,20 @@
-import { Slot, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Slot, SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
+SplashScreen.preventAutoHideAsync()
 export default function RoootLayout () {
+    const [fontsLoaded, error] = useFonts({
+        "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+    });
+    
+    useEffect(()=>{
+        if(error) throw error
+        if (fontsLoaded) SplashScreen.hideAsync()
+    },[fontsLoaded, error])
+
+    if (!fontsLoaded) return null
+    if (!fontsLoaded && !error) return null
     return (
         <Stack>
             <Stack.Screen name='(tabs)' options={{headerShown: false}}/>
